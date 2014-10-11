e9ab633363be4ac69bb9f0d88b6c0f56493984fd
Blake Lawson <blakemlaws@gmail.com>
2014-10-11 18:09:09 -0400

    DB schema done except foreign key relationships

--- setup/schema.php
+++ setup/schema.php
@@ -12,15 +12,54 @@
 
 	// Define User table
 	$User = new model("User");
-	$User->add_column("id", "INT NOT NULL AUTO_INCREMENT PRIMARY KEY");
+	$User->add_column("id", "INT NOT NULL AUTO_INCREMENT");
+	$User->add_column("PRIMARY KEY", "(id)");
 	$User->add_column("first_name", "VARCHAR(64) NOT NULL");
 	$User->add_column("last_name", "VARCHAR(64) NOT NULL");
-	$User->add_column("email", "");
+	$User->add_column("email", "VARCHAR(64)");
+	$User->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	$User->add_column("birthday", "DATETIME");
+	$User->add_column("gender", "CHAR(1)");
+	$User->add_column("last_active", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	// $User->add_column(); // Events posted
+	// $User->add_column(); // Groups
+	// $User->add_column(); // Location
+	$User->create_table();
+
+	// Define Tag table
+	$Tag = new model("Tag");
+	$Tag->add_column("id", "INT NOT NULL AUTO_INCREMENT");
+	$Tag->add_column("PRIMARY KEY", "(id)");
+	$Tag->add_column("val", "VARCHAR(32)");
+	$Tag->create_table();
 
 
 	// Define Event table
 	$Event = new model("Event");
+	$Event->add_column("id", "INT NOT NULL AUTO_INCREMENT");
+	$Event->add_column("PRIMARY KEY", "(id)");
+	$Event->add_column("title", "VARCHAR(64) NOT NULL");
+	$Event->add_column("description", "VARCHAR(256) NOT NULL");
+	$Event->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	$Event->add_column("time", "DATETIME NOT NULL");
+	// $Event->add_column(""); // Location
+	// $Event->add_column(""); // Type
+	// $Event->add_column(""); // Tag
+	$Event->add_column("min_price", "INT");
+	$Event->add_column("max_price", "INT");
+	$Event->add_column("public", "BOOL NOT NULL");
+	// $Event->add_column(""); // Age group
+	// $Event->add_column("attendees", ); // People who have RSVPed
+	$Event->create_table();
 
-
-
+	// Define Group table
+	$Group = new model("Group");
+	$Group->add_column("id", "INT NOT NULL AUTO_INCREMENT");
+	$Group->add_column("PRIMARY KEY", "(id)");
+	// $Group->add_column("creator", "") // Group creator
+	$Group->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	$Group->add_column("last_active", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	// $Group->add_column(""); // Users in group
+	// $Group->add_column(""); // Itinerary (Events)
+	$Group->create_table();
 ?>
