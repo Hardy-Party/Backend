27706a4f91ec820e2f6525212b8dbc6438dabbb3
Blake Lawson <blakemlaws@gmail.com>
2014-10-12 02:40:28 -0400

    d

--- setup/model.php
+++ setup/model.php
@@ -65,8 +65,15 @@
 
 			if ($result)
 			{
-				// Delete existing tables by this name
-				mysql_query("DROP TABLE $this->title;", $con) or die(mysql_eror());
+				try 
+				{
+					// Delete existing tables by this name
+					mysql_query("DROP TABLE $this->title;", $con) or die(mysql_eror());
+				} 
+				catch (Exception $e)
+				{
+					echo nl2br("$e->getMessage\n");
+				}
 			}
 
 			// Execute command
