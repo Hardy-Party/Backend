fd114199199d6377a8e7e2328980043b002403a1
Blake Lawson <blakemlaws@gmail.com>
2014-10-11 23:52:26 -0400

    d

--- setup/model.php
+++ setup/model.php
@@ -61,6 +61,8 @@
 
 			print(nl2br($query . "\n"));
 
+			$result = mysql_query("SELECT * FROM $this->title", $con) or die(mysql_error());
+
 			// Execute command
 			mysql_query($query, $con) or die(mysql_error());
 		}
--- setup/schema.php
+++ setup/schema.php
@@ -13,15 +13,14 @@
 	// Define User table
 	$User = new model("User");
 	$User->add_column("user_id", "INT NOT NULL PRIMARY KEY AUTO_INCREMENT");
-	// $User->add_column("PRIMARY KEY", "(user_id)");
-	// $User->add_column("first_name", "VARCHAR(64) NOT NULL");
-	// $User->add_column("last_name", "VARCHAR(64) NOT NULL");
-	// $User->add_column("email", "VARCHAR(64)");
-	// $User->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
-	// $User->add_column("birthday", "DATETIME");
-	// $User->add_column("gender", "CHAR(1)");
-	// $User->add_column("last_active", "DATETIME DEFAULT CURRENT_TIMESTAMP");
-
+	$User->add_column("PRIMARY KEY", "(user_id)");
+	$User->add_column("first_name", "VARCHAR(64) NOT NULL");
+	$User->add_column("last_name", "VARCHAR(64) NOT NULL");
+	$User->add_column("email", "VARCHAR(64)");
+	$User->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	$User->add_column("birthday", "DATETIME");
+	$User->add_column("gender", "CHAR(1)");
+	$User->add_column("last_active", "DATETIME DEFAULT CURRENT_TIMESTAMP");
 	// $User->add_column(); // Events posted
 	// $User->add_column(); // Groups
 	// $User->add_column(); // Location
@@ -30,28 +29,25 @@
 	// Define Tag table
 	$Tag = new model("Tag");
 	$Tag->add_column("tag_id", "INT NOT NULL PRIMARY KEY AUTO_INCREMENT");
-	// $Tag->add_column("PRIMARY KEY", "(tag_id)");
-	// $Tag->add_column("val", "VARCHAR(32)");
+	$Tag->add_column("PRIMARY KEY", "(tag_id)");
+	$Tag->add_column("val", "VARCHAR(32)");
 	$Tag->create_table();
 
 
 	// Define Event table
 	$Event = new model("Event");
 	$Event->add_column("event_id", "INT NOT NULL PRIMARY KEY AUTO_INCREMENT");
-	// $Event->add_column("PRIMARY KEY", "(event_id)");
-	// $Event->add_column("title", "VARCHAR(64) NOT NULL");
-	// $Event->add_column("description", "VARCHAR(256) NOT NULL");
-	// $Event->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
-	// $Event->add_column("time", "DATETIME NOT NULL");
-
+	$Event->add_column("PRIMARY KEY", "(event_id)");
+	$Event->add_column("title", "VARCHAR(64) NOT NULL");
+	$Event->add_column("description", "VARCHAR(256) NOT NULL");
+	$Event->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	$Event->add_column("time", "DATETIME NOT NULL");
 	// $Event->add_column(""); // Location
 	// $Event->add_column(""); // Type
 	// $Event->add_column(""); // Tag
-
-	// $Event->add_column("min_price", "INT");
-	// $Event->add_column("max_price", "INT");
-	// $Event->add_column("public", "BOOL NOT NULL");
-
+	$Event->add_column("min_price", "INT");
+	$Event->add_column("max_price", "INT");
+	$Event->add_column("public", "BOOL NOT NULL");
 	// $Event->add_column(""); // Age group
 	// $Event->add_column("attendees", ); // People who have RSVPed
 	$Event->create_table();
@@ -59,13 +55,10 @@
 	// Define Group table
 	$Group = new model("Group");
 	$Group->add_column("group_id", "INT NOT NULL PRIMARY KEY AUTO_INCREMENT");
-	// $Group->add_column("PRIMARY KEY", "(group_id)");
-
+	$Group->add_column("PRIMARY KEY", "(group_id)");
 	// $Group->add_column("creator", "") // Group creator
-
-	// $Group->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
-	// $Group->add_column("last_active", "DATETIME DEFAULT CURRENT_TIMESTAMP");
-
+	$Group->add_column("date_created", "DATETIME DEFAULT CURRENT_TIMESTAMP");
+	$Group->add_column("last_active", "DATETIME DEFAULT CURRENT_TIMESTAMP");
 	// $Group->add_column(""); // Users in group
 	// $Group->add_column(""); // Itinerary (Events)
 	$Group->create_table();
